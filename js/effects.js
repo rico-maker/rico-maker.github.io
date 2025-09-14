b51200e22ee37c85a940ff03cb8486b33e8b3465
Enrico <130112655+rico-maker@users.noreply.github.com>
2025-09-13 22:05:26 -0300

    Update effects.js

--- js/effects.js
+++ js/effects.js
@@ -1,5 +1,4 @@
-var r2PitchShifter = null;
-var audioContext = new AudioContext;
+var audioContext = new AudioContext();
 var audioInput = null,
     realAudioInput = null,
     effectInput = null,
@@ -7,142 +6,69 @@ var audioInput = null,
     dryGain = null,
     outputMix = null,
     currentEffectNode = null,
-    reverbBuffer = null,
-    dtime = null,
-    dregen = null,
-    lfo = null,
-    cspeed = null,
-    cdelay = null,
-    cdepth = null,
-    scspeed = null,
-    scldelay = null,
-    scrdelay = null,
-    scldepth = null,
-    scrdepth = null,
-    fldelay = null,
-    flspeed = null,
-    fldepth = null,
-    flfb = null,
-    sflldelay = null,
-    sflrdelay = null,
-    sflspeed = null,
-    sflldepth = null,
-    sflrdepth = null,
-    sfllfb = null,
-    sflrfb = null,
-    rmod = null,
-    mddelay = null,
-    mddepth = null,
-    mdspeed = null,
-    lplfo = null,
-    lplfodepth = null,
-    lplfofilter = null,
-    awFollower = null,
-    awDepth = null,
-    awFilter = null,
-    ngFollower = null,
-    ngGate = null,
-    bitCrusher = null,
-    btcrBits = 16,   // between 1 and 16
-    btcrNormFreq = 1; // between 0.0 and 1.0
+    reverbBuffer = null;
 
+var lpInputFilter = null;
 var rafID = null;
-var analyser1;
-var analyserView1;
-var constraints = 
-{
-  audio: {
-      optional: [{ echoCancellation: false }]
-  }
-};
+var analyser1, analyser2;
+var analyserView1, analyserView2;
 
-function convertToMono( input ) {
+// Pitch bend final via R2
+var r2PitchNode = null;
+var gamepadIndex = null;
+
+// Funções auxiliares
+function convertToMono(input) {
     var splitter = audioContext.createChannelSplitter(2);
     var merger = audioContext.createChannelMerger(2);
-
-    input.connect( splitter );
-    splitter.connect( merger, 0, 0 );
-    splitter.connect( merger, 0, 1 );
+    input.connect(splitter);
+    splitter.connect(merger, 0, 0);
+    splitter.connect(merger, 0, 1);
     return merger;
 }
 
-window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
-window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame;
-
-function cancelAnalyserUpdates() {
-    if (rafID)
-        window.cancelAnimationFrame( rafID );
-    rafID = null;
-}
-
-function updateAnalysers(time) {
-    analyserView1.doFrequencyAnalysis( analyser1 );
-    analyserView2.doFrequencyAnalysis( analyser2 );
-    
-    rafID = window.requestAnimationFrame( updateAnalysers );
-}
-
-var lpInputFilter=null;
-
-// this is ONLY because we have massive feedback without filtering out
-// the top end in live speaker scenarios.
 function createLPInputFilter() {
     lpInputFilter = audioContext.createBiquadFilter();
     lpInputFilter.frequency.value = 2048;
     return lpInputFilter;
 }
 
-
+// Toggle Mono com proteção contra null
 function toggleMono() {
+    if(!audioInput || !realAudioInput) return;
     if (audioInput != realAudioInput) {
         audioInput.disconnect();
         realAudioInput.disconnect();
         audioInput = realAudioInput;
     } else {
         realAudioInput.disconnect();
-        audioInput = convertToMono( realAudioInput );
+        audioInput = convertToMono(realAudioInput);
     }
-
     createLPInputFilter();
     lpInputFilter.connect(dryGain);
     lpInputFilter.connect(analyser1);
     lpInputFilter.connect(effectInput);
 }
 
-var useFeedbackReduction = true;
-
 function gotStream(stream) {
-    // Create an AudioNode from the stream.
-//    realAudioInput = audioContext.createMediaStreamSource(stream);
     var input = audioContext.createMediaStreamSource(stream);
+    audioInput = convertToMono(input);
 
-/*
-    realAudioInput = audioContext.createBiquadFilter();
-    realAudioInput.frequency.value = 60.0;
-    realAudioInput.type = realAudioInput.NOTCH;
-    realAudioInput.Q = 10.0;
-
-    input.connect( realAudioInput );
-*/
-    audioInput = convertToMono( input );
-
-    if (useFeedbackReduction) {
-        audioInput.connect( createLPInputFilter() );
-        audioInput = lpInputFilter;
-        
-    }
-    // create mix gain nodes
+    // Cria mix de efeitos
     outputMix = audioContext.createGain();
     dryGain = audioContext.createGain();
     wetGain = audioContext.createGain();
     effectInput = audioContext.createGain();
+
     audioInput.connect(dryGain);
     audioInput.connect(analyser1);
     audioInput.connect(effectInput);
     dryGain.connect(outputMix);
     wetGain.connect(outputMix);
-    outputMix.connect( audioContext.destination);
+    outputMix.connect(audioContext.destination);
     outputMix.connect(analyser2);
+
+    // === Pitch bend final via R2 ===
     r2PitchNode = audioContext.createScriptProcessor(1024, 1, 1);
     effectInput.connect(r2PitchNode);
     r2PitchNode.connect(outputMix);
@@ -167,95 +93,53 @@ function gotStream(stream) {
     updateAnalysers();
 }
 
-function changeInput(){
-  if (!!window.stream) {
-    window.stream.stop();
-  }
-  var audioSelect = document.getElementById("audioinput");
-  var audioSource = audioSelect.value;
-  constraints.audio.optional.push({sourceId: audioSource});
+// Gamepad
+window.addEventListener("gamepadconnected", (e) => { gamepadIndex = e.gamepad.index; });
+window.addEventListener("gamepaddisconnected", (e) => { if (gamepadIndex === e.gamepad.index) gamepadIndex = null; });
 
-  navigator.getUserMedia(constraints, gotStream, function(e) {
-            alert('Error getting audio');
-            console.log(e);
-        });
+// Analisadores
+function cancelAnalyserUpdates() {
+    if (rafID) window.cancelAnimationFrame(rafID);
+    rafID = null;
 }
 
-function gotSources(sourceInfos) {
-    var audioSelect = document.getElementById("audioinput");
-    while (audioSelect.firstChild)
-        audioSelect.removeChild(audioSelect.firstChild);
-
-    for (var i = 0; i != sourceInfos.length; ++i) {
-        var sourceInfo = sourceInfos[i];
-        if (sourceInfo.kind === 'audioinput') {
-            var option = document.createElement("option");
-            option.value = sourceInfo.id;
-            option.text = sourceInfo.label || 'input ' + (audioSelect.length + 1);
-            audioSelect.appendChild(option);
-        }
-    }
-    audioSelect.onchange = changeInput;
+function updateAnalysers(time) {
+    analyserView1.doFrequencyAnalysis(analyser1);
+    analyserView2.doFrequencyAnalysis(analyser2);
+    rafID = window.requestAnimationFrame(updateAnalysers);
 }
 
+// Crossfade
+function crossfade(value) {
+    var gain1 = Math.cos(value * 0.5 * Math.PI);
+    var gain2 = Math.cos((1.0 - value) * 0.5 * Math.PI);
+    dryGain.gain.value = gain1;
+    wetGain.gain.value = gain2;
+}
+
+// Inicialização de áudio
 function initAudio() {
-    var irRRequest = new XMLHttpRequest();
-    irRRequest.open("GET", "sounds/cardiod-rear-levelled.wav", true);
-    irRRequest.responseType = "arraybuffer";
-    irRRequest.onload = function() {
-        audioContext.decodeAudioData( irRRequest.response, 
-            function(buffer) { reverbBuffer = buffer; } );
-    }
-    irRRequest.send();
-
-    o3djs.require('o3djs.shader');
-
     analyser1 = audioContext.createAnalyser();
     analyser1.fftSize = 1024;
     analyser2 = audioContext.createAnalyser();
     analyser2.fftSize = 1024;
 
     analyserView1 = new AnalyserView("view1");
-    analyserView1.initByteBuffer( analyser1 );
+    analyserView1.initByteBuffer(analyser1);
     analyserView2 = new AnalyserView("view2");
-    analyserView2.initByteBuffer( analyser2 );
+    analyserView2.initByteBuffer(analyser2);
 
     if (!navigator.getUserMedia)
-        return(alert("Error: getUserMedia not supported!"));
+        return alert("Error: getUserMedia not supported!");
 
-    navigator.getUserMedia(constraints, gotStream, function(e) {
-            alert('Error getting audio');
-            console.log(e);
-        });
-
-    navigator.mediaDevices.enumerateDevices().then(gotSources);
-    document.getElementById("effect").onchange=changeEffect;
+    navigator.getUserMedia({audio: {optional: [{ echoCancellation: false }]}}, gotStream, function(e){
+        alert('Error getting audio'); console.log(e);
+    });
 }
 
+// Chamadas de inicialização
+window.addEventListener('load', initAudio);
 
-function keyPress(ev) {
-    var oldEffect = document.getElementById("effect").selectedIndex;
-    var newEffect = oldEffect;
-    switch (ev.keyCode) {
-      case 50: // 'r'
-        newEffect = 1;
-        break;
-      case 49: // 'c'
-        newEffect = 8;
-        break;
-      case 51: // 'p'
-        newEffect = 10;
-        break;
-      default:
-        console.log(ev.keyCode);
-    }
-    if (newEffect != oldEffect) {
-        document.getElementById("effect").selectedIndex = newEffect;
-        changeEffect();
-    }
-}
-
-window.addEventListener('load', initAudio );
 
 let gamepadIndex = null;
 
